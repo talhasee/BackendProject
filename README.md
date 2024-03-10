74bc132f557ec703ac244ba2700bf312578163af
Pratham Koli <prathamkoli8259@gmail.com>
2024-03-10 20:17:18 +0530

    added remaining video controllers

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 - Figma Link -> [Link](https://www.figma.com/file/shmxWL5FKRO5GNOPPopBg6/PLAY?type=design&node-id=0-1&mode=design&t=Z719S6WJeUn79NcN-0)
 - Model Link -> [Link](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
+
+
+Here's a list of some common HTTP status codes along with their corresponding meanings:
+
+1. **200 OK**: The request was successful.
+2. **201 Created**: The request has been fulfilled, resulting in the creation of a new resource.
+3. **204 No Content**: The server successfully processed the request, but there is no content to return.
+4. **400 Bad Request**: The server cannot process the request due to a client error (e.g., malformed syntax).
+5. **401 Unauthorized**: The request requires user authentication. The client must authenticate itself to get the requested response.
+6. **403 Forbidden**: The server understood the request but refuses to authorize it. The client does not have permission to access the requested resource.
+7. **404 Not Found**: The server cannot find the requested resource.
+8. **405 Method Not Allowed**: The method specified in the request is not allowed for the resource identified by the request URI.
+9. **408 Request Timeout**: The server timed out waiting for the request.
+10. **429 Too Many Requests**: The user has sent too many requests in a given amount of time.
+11. **500 Internal Server Error**: The server encountered an unexpected condition that prevented it from fulfilling the request.
+12. **502 Bad Gateway**: The server received an invalid response from an upstream server while trying to fulfill the request.
+13. **503 Service Unavailable**: The server is currently unable to handle the request due to temporary overloading or maintenance of the server.
+14. **504 Gateway Timeout**: The server did not receive a timely response from the upstream server while attempting to load the requested resource.
+
+These are some of the most commonly encountered HTTP status codes, but there are many more defined in the HTTP specification. Each status code provides information about the result of the request, allowing clients and servers to communicate effectively about the outcome of each request.
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,11 +1,14 @@
 import { apiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { deleteFromCloudinary, uploadOnCloudinary } from "../utils/cloudinary.js";
 import { Video } from "../models/video.models.js";
+import {Like} from "../models/likes.models.js";
+import {Comment} from "../models/comments.models.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import mongoose, { isValidObjectId } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 import { User } from "../models/user.models.js";
+import { extractPublicId } from "cloudinary-build-url";
 
 
 const publishAVideo = asyncHandler(async (req, res) => {
@@ -210,8 +213,183 @@ const getVideoById = asyncHandler(async (req, res) => {
 
 });
 
+const updateVideo = asyncHandler(async (req, res) => {
+    const [title, description] = req.body;
+    const [videoId] = req.params;
+
+    if(!isValidObjectId(videoId)){
+        throw new apiError(400, "Invalid Video ID");
+    }
+
+    const video = await Video.findById(videoId);
+    
+    if(!video){
+        throw new apiError(404, "Video not found");
+    }
+    
+    if(video?.owner.toString() != req.user?._id.toString()){
+        throw new apiError(400, "Permission denied");
+    }
+    
+    const thumbnailLocalPath = req.file?.path;
+
+    if(!title && !description && !thumbnailLocalPath){
+        throw new apiError(400, "Atleast one value is required (Title or description or thumbnailLocalPath)");
+    }
+
+    const oldTitle = video?.title;
+    const oldDescription = video?.description;
+    const oldThumbnail = video?.thumbnail;
+
+    title = title ? title : oldTitle;
+    description = description ? description : oldDescription;
+
+    //if we have user thumbnail image for updation then update url else we 
+    //keep url as it is
+    if(thumbnailLocalPath){
+        const newThumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+        if(!newThumbnail){
+            throw new apiError(400, "Error in uploading");
+        }
+
+        oldThumbnail = newThumbnail;
+    }
+
+    const updatedVideo = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $set: {
+                title,
+                description,
+                oldThumbnail
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    if(!updatedVideo){
+        throw new apiError(500, "Error in updating video details Try again");
+    }else{
+        await deleteFromCloudinary(extractPublicId(oldThumbnail));
+    }
+
+    return res
+    .status(200)
+    .json(
+        200,
+        updatedVideo,
+        "Video updated Successfully"
+    )
+
+    
+});
+
+const deleteVideo = asyncHandler(async(req, res) => {
+    const videoId = req.params;
+    
+    if(!isValidObjectId(videoId)){
+        throw new apiError(400, "Invalid Video");
+    }
+
+    const video = await Video.findById(videoId);
+
+    if(!video){
+        throw new apiError(404, "Video not found");
+    }
+
+    if(video?.owner._id.toString() != req.user?._id){
+        throw new apiError(403, "Permission denied")
+    }
+
+    const videoFile = video?.videoFile;
+    const thumbnail = video?.thumbnail;
+
+    const videoDeleted = await Video.findByIdAndDelete(video?._id);
+
+    if(!videoDeleted){
+        throw new apiError(500, "Unable to delete video Try again");
+    }
+
+    await deleteFromCloudinary(extractPublicId(videoFile));
+    await deleteFromCloudinary(extractPublicId(thumbnail));
+
+    Like.deleteMany(
+        {
+            video: video?._id
+        }
+    );
+
+    Comment.deleteMany(
+        {
+            video: video?._id
+        }
+    );
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(
+            200, 
+            {},
+            "Video deleted successfully"
+        )
+    );
+
+});
+
+const togglePublishStatus = asyncHandler(async (req, res) => {
+    const videoId = req.params;
+
+    if(!isValidObjectId(videoId)){
+        throw new apiError(400, "Invalid video Id");
+    }
+
+    const video = await Video.findById(videoId);
+
+    if(!video){
+        throw new apiError(404, "Video not found");
+    }
+
+    if(video._id.toString() != req.user?._id.toString()){
+        throw new apiError(403, "Permission denied");
+    }
+
+    const updatedVideo = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $set: {
+                isPublished: !video.isPublished
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    if(!updateVideo){
+        throw new apiError(500, "Failed to toggle publish status");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            updateVideo,
+            "Video publish status toggled successfully"
+        )
+    );
+    
+});
+
+
 
 export {
     publishAVideo,
     getVideoById,
+    updateVideo,
+    deleteVideo,
+    togglePublishStatus,
 };  
