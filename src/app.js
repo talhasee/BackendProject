ec173746985d2b5f39a982f134597aff7faec909
Pratham Koli <prathamkoli8259@gmail.com>
2024-04-03 21:05:39 +0530

    Increase payload limit

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(
 //
 app.use(
   express.json({
-    limit: "16Kb",
+    limit: "60mb",
   })
 );
 
@@ -22,7 +22,7 @@ app.use(
 app.use(
   express.urlencoded({
     extended: true,
-    limit: "16Kb",
+    limit: "60mb",
   })
 );
 
