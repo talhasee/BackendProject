9a902a2e7a0332fe93b5be69c8ef670da0d51006
Pratham Koli <prathamkoli8259@gmail.com>
2024-12-17 19:08:31 +0530

    Bug in video deletion

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -298,7 +298,7 @@ const updateVideo = asyncHandler(async (req, res) => {
 
 //
 const deleteVideo = asyncHandler(async(req, res) => {
-    const videoId = req.params;
+    const {videoId} = req.params;
     
     if(!isValidObjectId(videoId)){
         throw new apiError(400, "Invalid Video");
